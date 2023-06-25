a44a19ec414c392b377aba90e3200c2619c16d52
CocoBadman <cocobadman@pop-os.localdomain>
2023-06-24 19:38:53 -0700

    Correct date format and refactor

--- interview/weather.py
+++ interview/weather.py
@@ -4,8 +4,7 @@ import pandas as pd
 TIMESTAMP_COL = "Measurement Timestamp"
 STATION_NAME_COL = "Station Name"
 TEMP_COL = "Air Temperature"
-DATE_FORMAT = "mixed"
-FORMAT = "%Y-%m-%d %H:%M:%S"
+INPUT_DATE_FORMAT = "mixed"
 CHUNK_SIZE = 1000
 
 # Intermediary column to help with batching aggregation
@@ -13,6 +12,7 @@ EXTRA_TIMESTAMP_COL = "Duplicate Timestamp"
 
 # Output csv constants
 DATE_COL_NAME = "Date"
+OUTPUT_DATE_FORMAT = "%m/%d/%Y"
 MIN_COL_NAME = "Min Temp"
 MAX_COL_NAME = "Max Temp"
 FIRST_COL_NAME = "First Temp"
@@ -24,7 +24,7 @@ def process_csv(reader, writer):
     df_chunks = pd.read_csv(
         reader,
         chunksize=CHUNK_SIZE,
-        date_format=DATE_FORMAT,
+        date_format=INPUT_DATE_FORMAT,
         parse_dates=[TIMESTAMP_COL],
         usecols=[TIMESTAMP_COL, STATION_NAME_COL, TEMP_COL],
     )
@@ -53,12 +53,8 @@ def transform_df_batch(df_chunks):
         chunk[TEMP_COL] = pd.to_numeric(chunk[TEMP_COL], downcast="float")
 
         # Split by station name and resample by day
-        group_station_resample_day = chunk.groupby(
-            [
-                STATION_NAME_COL,
-                pd.Grouper(key=TIMESTAMP_COL, freq="D"),
-            ]
-        )
+        group_station_resample_day = groupby_name_resample_day(
+            chunk, STATION_NAME_COL, TIMESTAMP_COL)
 
         # Combine groups to get desired aggregations
         min_chunk = group_station_resample_day.min().rename(
@@ -81,32 +77,51 @@ def transform_df_batch(df_chunks):
 
     # After all batches have been aggregated, group by extra timestamp column
     # to ensure that first and last data is correct
-    result_min = min_acc_df.groupby(
-        [STATION_NAME_COL, pd.Grouper(key=EXTRA_TIMESTAMP_COL, freq="D")]
-    ).min()
-    result_max = max_acc_df.groupby(
-        [STATION_NAME_COL, pd.Grouper(key=EXTRA_TIMESTAMP_COL, freq="D")]
-    ).max()
-    result_first = first_acc_df.groupby(
-        [STATION_NAME_COL, pd.Grouper(key=EXTRA_TIMESTAMP_COL, freq="D")]
-    ).first()
-    result_last = last_acc_df.groupby(
-        [STATION_NAME_COL, pd.Grouper(key=EXTRA_TIMESTAMP_COL, freq="D")]
-    ).last()
+    result_min = groupby_name_resample_day(
+        min_acc_df, STATION_NAME_COL, EXTRA_TIMESTAMP_COL).min()
+    result_max = groupby_name_resample_day(
+        max_acc_df, STATION_NAME_COL, EXTRA_TIMESTAMP_COL).max()
+    result_first = groupby_name_resample_day(
+        first_acc_df, STATION_NAME_COL, EXTRA_TIMESTAMP_COL).first()
+    result_last = groupby_name_resample_day(
+        last_acc_df, STATION_NAME_COL, EXTRA_TIMESTAMP_COL).last()
 
     temp_dataframes = [result_min, result_max, result_first, result_last]
 
     # Glue dataframes horizonatally
     concated_temps_df = pd.concat(temp_dataframes, axis=1).reset_index()
 
-    # Prepare for desired CSV format
-    result_df = concated_temps_df.set_index([STATION_NAME_COL]).rename(
-        {EXTRA_TIMESTAMP_COL: DATE_COL_NAME}, axis=1
-    )
+    result_df = prepare_for_output_format(
+        concated_temps_df, EXTRA_TIMESTAMP_COL)
 
     return result_df
 
 
+# Group data by the name of a column and resample data by day
+def groupby_name_resample_day(
+        df: pd.DataFrame | pd.Series, name_col: str, date_col: str):
+    return df.groupby(
+        [
+            name_col,
+            pd.Grouper(key=date_col, freq="D"),
+        ]
+    )
+
+
+# Prepare for desired CSV format
+def prepare_for_output_format(
+        df: pd.DataFrame, time_col_name: str) -> pd.DataFrame:
+
+    df = df.set_index([STATION_NAME_COL]).rename(
+        {time_col_name: DATE_COL_NAME}, axis=1
+    )
+    df[DATE_COL_NAME] = df[DATE_COL_NAME].dt.strftime(
+        OUTPUT_DATE_FORMAT)
+
+    return df
+
+
+# Transform without batch. Used for testing
 def transform_df(input_df):
     # ensure float output for all temp data
     input_df[TEMP_COL] = pd.to_numeric(input_df[TEMP_COL], downcast="float")
@@ -135,9 +150,6 @@ def transform_df(input_df):
     # Glue dataframes horizonatally
     concated_temps_df = pd.concat(temp_dataframes, axis=1).reset_index()
 
-    # Prepare for desired CSV format
-    result_df = concated_temps_df.set_index(STATION_NAME_COL).rename(
-        {TIMESTAMP_COL: DATE_COL_NAME}, axis=1
-    )
+    result_df = prepare_for_output_format(concated_temps_df, TIMESTAMP_COL)
 
     return result_df
